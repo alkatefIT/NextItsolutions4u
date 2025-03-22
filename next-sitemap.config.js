138ce36fbd079d193d26f6a8aba12591ab858814
alkatefIT <web.alkatef@gmail.com>
2025-03-23 01:39:26 +0530

    siitemap updates

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,8 +2,12 @@
 module.exports = {
     siteUrl: "https://next-itsolutions4u.vercel.app/", // Replace with your actual domain
     generateRobotsTxt: true, // Generates robots.txt
+    sitemapSize: 5000,
     exclude: ["/admin", "/dashboard"], // Exclude pages if needed
     changefreq: "daily",
     priority: 0.8,
+    generateIndexSitemap: true, // 🚀 Disables index sitemap, merges into one file
+
   };
   
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,5 @@
 import HomePage from "./HomePage";
 
-export const metadata = {
-  title:
-    "UAE's Trusted IT Partner, every business for one solution in al katef",
-  description:
-    "Al Katef Advertising Agency is a dynamic and creative firm specializing in innovative marketing solutions.we focus on delivering impactful advertising campaigns, we are the best leading IT solutions provider in UAE",
-};
-
 
 export default function Page() {
   return <HomePage />;
