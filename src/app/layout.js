df5a32a6ac49aac4f1e90f9dbc78e69cf9483777
alkatefIT <web.alkatef@gmail.com>
2025-04-09 19:39:46 +0530

    meta title,desc and keyword-home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,28 @@ import "./globals.css";
 import Footer from "../components/Footer/Footer";
 import Whatsapp from "@/components/whatsapp/Whatsapp";
 
-
 export const metadata = {
   title:
-    "UAE's Trusted IT Partner, every business for one solution in al katef",
+    "Digital Marketing Agency in UAE | Digital Marketing Agency in Abu Dhabi - Al Katef",
   description:
-    "Al Katef Advertising Agency is a dynamic and creative firm specializing in innovative marketing solutions.we focus on delivering impactful advertising campaigns, we are the best leading IT solutions provider in UAE",
-    keywords:"Al Katef Advertising Agency, marketing solutions, advertising campaigns, IT solutions provider in UAE, creative firm UAE, digital marketing UAE, UAE IT services",
+    "Looking for the best digital marketing agency in UAE and Abu Dhabi? Al Katef offers exceptional web design, SEO, eCommerce, social media, ads, and content creation to transform your online presence.",
+  keywords: [
+    "Al Katef Advertising Agency is a dynamic and creative firm specializing in innovative marketing solutions.we focus on delivering impactful advertising campaigns, we are the best leading IT solutions provider in UAE",
+    "digital marketing agency in uae",
+    "best digital marketing agency in uae",
+    "best digital marketing company in uae",
+    "best marketing agency in uae",
+    "best digital marketing agency uae",
+    "digital marketing abu dhabi",
+    "digital marketing agency in abu dhabi",
+    "best digital marketing agency in abu dhabi",
+    "social media agencies in abu dhabi",
+    "abu dhabi digital marketing company",
+    "best digital marketing abu dhabi",
+    "best digital marketing company in abu dhabi",
+    "best social media agencies in abu dhabi",
+    "digital marketing services in abu dhabi",
+  ].join(", "),
   alternates: {
     canonical: "https://www.itsolutions4u.co",
   },
@@ -19,16 +34,11 @@ export const metadata = {
   },
 };
 
-
-
 export default function RootLayout({ children }) {
-
-
-
   return (
     <html lang="en">
       <body>
-        <Whatsapp/>
+        <Whatsapp />
         <Navbar />
         {children}
         <Footer />
